27395001d8fb4541baca0fd4c87adb99f2d12924
XMLHexagram <xml.hexagram@gmail.com>
2024-01-18 00:07:36 +0800

    init

---
+++ Casks/parallels_19_2.rb
@@ -0,0 +1,74 @@
+cask "parallels_19_2" do
+  version "19.2.0"
+  sha256  "895ac8ed933fb58e59ba64e74078468b399d34327d23cb0ffc439515ca42438f",
+
+  url "https://github.com/pot-app/pot-desktop/releases/download/#{version}/pot_#{version}_#{arch}.dmg"
+  name "Parallels Desktop"
+  desc "Desktop virtualization software"
+  homepage "https://github.com/XMLHexagram/homebrew-tap"
+
+  app "pot.app"
+  binary "#{appdir}/pot.app/Contents/MacOS/pot"
+
+  app "Parallels Desktop.app"
+
+  preflight do
+    system_command "chflags",
+                   args: ["nohidden", "#{staged_path}/Parallels Desktop.app"]
+    system_command "xattr",
+                   args: ["-d", "com.apple.FinderInfo", "#{staged_path}/Parallels Desktop.app"]
+  end
+
+  postflight do
+    system_command "#{appdir}/Parallels Desktop.app/Contents/MacOS/inittool",
+                   args: ["init"],
+                   sudo: true
+  end
+
+  uninstall_preflight do
+    set_ownership "#{appdir}/Parallels Desktop.app"
+  end
+
+  uninstall signal: ["TERM", "com.parallels.desktop.console"],
+            delete: [
+              "/usr/local/bin/prl_convert",
+              "/usr/local/bin/prl_disk_tool",
+              "/usr/local/bin/prl_perf_ctl",
+              "/usr/local/bin/prlcore2dmp",
+              "/usr/local/bin/prlctl",
+              "/usr/local/bin/prlexec",
+              "/usr/local/bin/prlsrvctl",
+              "/Library/Preferences/Parallels",
+            ]
+
+  zap trash: [
+        "~/.parallels_settings",
+        "~/Applications (Parallels)",
+        "~/Library/Application Scripts/*.com.parallels.Desktop",
+        "~/Library/Application Scripts/com.parallels.desktop*",
+        "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.parallels.desktop.console.sfl*",
+        "~/Library/Caches/com.apple.helpd/Generated/com.parallels.desktop.console.help*",
+        "~/Library/Caches/com.parallels.desktop.console",
+        "~/Library/Caches/Parallels Software/Parallels Desktop",
+        "~/Library/Containers/com.parallels.desktop*",
+        "~/Library/Group Containers/*.com.parallels.Desktop",
+        "~/Library/Logs/parallels.log",
+        "~/Library/Parallels/Applications Menus",
+        "~/Library/Parallels/Parallels Desktop",
+        "~/Library/Preferences/com.parallels.desktop.console.LSSharedFileList.plist",
+        "~/Library/Preferences/com.parallels.desktop.console.plist",
+        "~/Library/Preferences/com.parallels.Parallels Desktop Events.plist",
+        "~/Library/Preferences/com.parallels.Parallels Desktop Statistics.plist",
+        "~/Library/Preferences/com.parallels.Parallels Desktop.plist",
+        "~/Library/Preferences/com.parallels.Parallels.plist",
+        "~/Library/Preferences/com.parallels.PDInfo.plist",
+        "~/Library/Preferences/Parallels",
+        "~/Library/Saved Application State/com.parallels.desktop.console.savedState",
+      ],
+      rmdir: [
+        "/Users/Shared/Parallels",
+        "~/Library/Caches/Parallels Software",
+        "~/Library/Parallels",
+        "~/Parallels",
+      ]
+end
